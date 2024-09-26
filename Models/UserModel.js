c5e650a1e3c5c79b2e5cea77e273785bf3e8c752
Pyaie Phyoe <pyaiephyoe@gmail.com>
2024-09-26 20:02:40 +0700

    install joi, modify UserModel with joi validation

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,25 +1,34 @@
 import mongoose from "mongoose";
+import Joi from "joi";
 
 const userModel = new mongoose.Schema(
   {
     username: {
       type: String,
+      minlenght: 3,
+      maxlength: 20,
       required: true,
     },
 
     email: {
       type: String,
+      minlenght: 5,
+      maxlength: 255,
       required: true,
       unique: true,
     },
 
     password: {
       type: String,
+      minlenght: 8,
+      maxlength: 20,
       required: true,
     },
 
     phone: {
-      type: Number,
+      type: String,
+      minlenght: 8,
+      maxlength: 20,
       required: true,
     },
 
@@ -32,6 +41,17 @@ const userModel = new mongoose.Schema(
   { timestamps: true }
 );
 
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    email: Joi.string().min(5).max(255).email().required(),
+    phone: Joi.String().min(8).max(20).required(),
+    password: Joi.String().min(8).max(30).required(),
+  });
+
+  return schema.validate(user);
+}
+
 const User = mongoose.model("User", userModel);
 
-export default User;
+export { User, validateUser };
