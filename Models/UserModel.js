a2ada9ff8b5e149a89ad031bc1a6d4dd7cc5d0f8
Pyaie Phyoe <pyaiephyoe@gmail.com>
2024-09-26 19:32:22 +0700

    create user model with mongoose schema

---
+++ Models/UserModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userModel = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    phone: {
+      type: Number,
+      required: true,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userModel);
+
+export default User;
