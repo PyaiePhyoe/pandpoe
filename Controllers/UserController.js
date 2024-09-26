43d8b37edaf4554edd58c7acd7733c8a3663ec79
Pyaie Phyoe <pyaiephyoe@gmail.com>
2024-09-26 21:11:08 +0700

    install bcrypt, create hashPassword function in UserModel, use it in UserController

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,4 +1,4 @@
-import { User, validateUser } from "../Models/UserModel.js";
+import { hashPassword, User, validateUser } from "../Models/UserModel.js";
 
 const createUser = async (req, res) => {
   const { error } = validateUser(req.body);
@@ -7,11 +7,13 @@ const createUser = async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User is already registered!");
 
+  const hashedPassword = await hashPassword(req.body.password);
+
   user = new User({
     username: req.body.username,
     email: req.body.email,
     phone: req.body.phone,
-    password: req.body.password,
+    password: hashedPassword,
   });
 
   await user.save();
--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import Joi from "joi";
+import bcrypt from "bcrypt";
 
 const userModel = new mongoose.Schema(
   {
@@ -21,7 +22,7 @@ const userModel = new mongoose.Schema(
     password: {
       type: String,
       minlenght: 8,
-      maxlength: 20,
+      maxlength: 255,
       required: true,
     },
 
@@ -46,12 +47,18 @@ function validateUser(user) {
     username: Joi.string().min(3).max(20).required(),
     email: Joi.string().min(5).max(255).email().required(),
     phone: Joi.string().min(8).max(20).required(),
-    password: Joi.string().min(8).max(30).required(),
+    password: Joi.string().min(8).max(255).required(),
   });
 
   return schema.validate(user);
 }
 
+async function hashPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+  return hashPassword;
+}
+
 const User = mongoose.model("User", userModel);
 
-export { User, validateUser };
+export { User, validateUser, hashPassword };
