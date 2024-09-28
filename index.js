bc53284380a45fba5ed1e71a50a9ac9104599790
Pyaie Phyoe <pyaiephyoe@gmail.com>
2024-09-28 16:49:52 +0700

    add path in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express, { urlencoded } from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import dotenv from "dotenv";
 import connectDB from "./db.js";
@@ -26,6 +27,9 @@ app.get("/", (req, res) => {
 app.use("/api/users", UserRoutes);
 app.use("/api/categories", CategoryRoutes);
 app.use("/api/upload", UploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 app.listen(port, () => {
   console.log(`Hello from server side, port ${port}!`);
